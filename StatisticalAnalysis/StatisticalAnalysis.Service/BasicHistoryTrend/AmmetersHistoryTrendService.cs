cb3f7f40ad92df4557035ea87dc170a9c6e8f822
decrula <decrula@gmail.com>
2015-06-16 12:16:11 +0800

    添加电表趋势分析

---
+++ StatisticalAnalysis/StatisticalAnalysis.Service/BasicHistoryTrend/AmmetersHistoryTrendService.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using SqlServerDataAdapter;
+using StatisticalAnalysis.Infrastruture.Configuration;
+
+namespace StatisticalAnalysis.Service.BasicHistoryTrend
+{
+    public class AmmetersHistoryTrendService
+    {
+        private readonly static string connectionString = ConnectionStringFactory.NXJCConnectionString;
+        private readonly static ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+
+        /// <summary>
+        /// 获取所有分厂的数据库
+        /// </summary>
+        /// <param name="organizationId">组织机构ID（公司级别）</param>
+        /// <returns></returns>
+        private static DataTable GetDatabaseByOrganizationId(string organizationId)
+        {
+            string sql = @"SELECT [D].[MeterDatabase]
+                             FROM [NXJC].[dbo].[system_Organization] AS [O] INNER JOIN
+                                  [NXJC].[dbo].[system_Database] AS [D] ON [O].[DatabaseID] = [D].[DatabaseID]
+                            WHERE [O].[LevelCode] LIKE (
+                                  SELECT [L].[LevelCode]
+	                                FROM [NXJC].[dbo].[system_Organization] AS [L]
+		                           WHERE [L].[organizationId] = @organizationId
+	                              ) + '%' AND
+                                  [O].[LevelType] = 'Factory'";
+
+            SqlParameter[] parameters = new SqlParameter[]{
+                new SqlParameter("OrganizationId", organizationId)
+            };
+
+            return dataFactory.Query(sql, parameters);
+        }
+
+        /// <summary>
+        /// 获取电表
+        /// </summary>
+        /// <param name="organizationId">组织机构ID（公司级别）</param>
+        /// <returns></returns>
+        public static DataTable GetAmmetersByOrganizationId(string organizationId)
+        {
+            DataTable factoryDatabases = GetDatabaseByOrganizationId(organizationId);
+
+            StringBuilder queryBuilder = new StringBuilder();
+
+            queryBuilder.Append(@"SELECT [O].[OrganizationID] AS [KeyId], [O].[OrganizationID] AS[OrganizationId], [O].[Name] AS [Name], '0' AS [ParentKeyId], '' AS [VariableId], 'Company' AS [LevelType], '' AS [TagTableName], '' AS [TagColumnName]
+                                    FROM [NXJC].[dbo].[system_Organization] AS [O]
+                                   WHERE [O].[OrganizationID] = @organizationId AND
+                                         [O].[LevelType] = 'Company'
+
+                                   UNION
+                             
+                                  SELECT [O].[OrganizationID] AS [KeyId], [O].[OrganizationID] AS[OrganizationId], [O].[Name] AS [Name], @organizationId AS [ParentKeyId], '' AS [VariableId], 'Factory' AS [LevelType], '' AS [TagTableName], '' AS [TagColumnName]
+                                    FROM [NXJC].[dbo].[system_Organization] AS [O]
+                                   WHERE [O].[LevelCode] LIKE (
+                                         SELECT [L].[LevelCode]
+	                                       FROM [NXJC].[dbo].[system_Organization] AS [L]
+		                                  WHERE [L].[OrganizationID] = @organizationId
+	                                     ) + '%' AND
+                                         [O].[LevelType] = 'Factory'
+                                  ");
+
+            foreach (DataRow factoryDatabase in factoryDatabases.Rows)
+            {
+                queryBuilder.Append("  UNION ");
+                queryBuilder.Append(@"SELECT DISTINCT [AC].[ElectricRoom] AS [KeyId], [AC].[OrganizationID] AS[OrganizationId], [AC].[ElectricRoom] AS [Name], [AC].[OrganizationID] AS [ParentKeyId], '' AS [VariableId], 'ElectricRoom' AS [LevelType], '' AS [TagTableName], '' AS [TagColumnName]
+                                        FROM [" + factoryDatabase[0].ToString().Trim() + @"].[dbo].[AmmeterContrast] AS [AC]
+                                  
+                                       UNION
+                                  
+                                      SELECT CAST(NEWID() AS VARCHAR(64)) AS [KeyId], [AC].[OrganizationID] AS[OrganizationId], RTRIM([AC].[AmmeterName]) AS [Name], [AC].[ElectricRoom] AS [ParentKeyId], RTRIM([AC].[AmmeterName]) AS [VariableId], 'Ammeters' AS [LevelType], '" + factoryDatabase[0].ToString().Trim() + @".dbo.HistoryAmmeter' AS [TagTableName], RTRIM([AC].[ElectricEnergyFieldNameSave]) AS [TagColumnName]
+                                        FROM [" + factoryDatabase[0].ToString().Trim() + @"].[dbo].[AmmeterContrast] AS [AC]
+                                    ");
+            }
+
+
+            SqlParameter[] parameters = new SqlParameter[]{
+                new SqlParameter("organizationId", organizationId)
+            };
+
+            return dataFactory.Query(queryBuilder.ToString(), parameters);
+        }
+
+
+        public static DataTable GetQuantityUsageGroup(DataTable tagTable, DateTime startTime, DateTime endTime, string myTimeInterval)
+        {
+            string queryString = @"SELECT {5}, '{0}' as OrganizationId, '{1}' as VariableId, SUM({2}) AS [Sum]
+                                     FROM {3}
+                                    WHERE [vDate] >= @startTime
+                                      AND [vDate] <= @endTime
+                                 GROUP BY {4}";
+            string m_TimeInterval = "";
+            string m_vDateColumn = "";
+            if (myTimeInterval == "10")             //按10分钟
+            {
+                m_vDateColumn = "YEAR([vDate]) as [Year], MONTH([vDate]) as [Month], DAY([vDate]) as [Day], DATEPART(HOUR, [vDate]) as [Hour], (DATEPART(mi,[vDate]) / 10) * 10 as [Minute]";
+                m_TimeInterval = "YEAR([vDate]), MONTH([vDate]), DAY([vDate]), DATEPART(HOUR, [vDate]), (DATEPART(mi,[vDate]) / 10) * 10";
+            }
+            else if (myTimeInterval == "20")         //按1小时
+            {
+                m_vDateColumn = "YEAR([vDate]) as [Year], MONTH([vDate]) as [Month], DAY([vDate]) as [Day], DATEPART(HOUR, [vDate]) as [Hour]";
+                m_TimeInterval = "YEAR([vDate]), MONTH([vDate]), DAY([vDate]), DATEPART(HOUR, [vDate])";
+            }
+            else if (myTimeInterval == "30")        //按天
+            {
+                m_vDateColumn = "YEAR([vDate]) as [Year], MONTH([vDate]) as [Month], DAY([vDate]) as [Day]";
+                m_TimeInterval = "YEAR([vDate]), MONTH([vDate]), DAY([vDate])";
+            }
+            StringBuilder queryBuilder = new StringBuilder();
+
+            foreach (DataRow dr in tagTable.Rows)
+            {
+                queryBuilder.Append(string.Format(queryString, dr["OrganizationId"], dr["VariableId"], dr["TagColumnName"], dr["TagTableName"], m_TimeInterval, m_vDateColumn));
+                queryBuilder.Append(" UNION ");
+            }
+
+            queryBuilder.Remove(queryBuilder.Length - 7, 7);
+
+            SqlParameter[] parameters = new SqlParameter[]{
+                new SqlParameter("startTime", startTime),
+                new SqlParameter("endTime", endTime)
+            };
+            string m_Sql = queryBuilder.ToString();
+
+            DataTable dt = dataFactory.Query(m_Sql, parameters);
+            return dt;
+        }
+    }
+}
---
+++ StatisticalAnalysis/StatisticalAnalysis.Web/UI_BasicHistoryTrend/HistoryTrend_Ammeters.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EasyUIJsonParser;
+using StatisticalAnalysis.Infrastruture.Utility;
+using StatisticalAnalysis.Service.BasicHistoryTrend;
+
+namespace StatisticalAnalysis.Web.UI_BasicHistoryTrend
+{
+    public partial class HistoryTrend_Ammeters : WebStyleBaseForEnergy.webStyleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ////////////////////调试用,自定义的数据授权
+#if DEBUG
+                List<string> m_DataValidIdItems = new List<string>() { "zc_nxjc_byc_byf" };
+                AddDataValidIdGroup("ProductionOrganization", m_DataValidIdItems);
+#elif RELEASE
+#endif
+                this.OrganisationTree_ProductionLine.Organizations = GetDataValidIdGroup("ProductionOrganization");                 //向web用户控件传递数据授权参数
+                this.OrganisationTree_ProductionLine.PageName = "HistoryTrend_Ammeters.aspx";                                       //向web用户控件传递当前调用的页面名称
+                this.OrganisationTree_ProductionLine.LeveDepth = 3;
+            }
+        }
+
+        [WebMethod]
+        public static string GetChartDataJson(string startTime, string endTime, string timeInterval, string tags)
+        {
+            // tagItems 格式：
+            // OrganizationID（组织机构ID）   TagTableName（数据库名+ .dbo. + 表名）   TagColumnName（字段名）  Name（名称）
+            DataTable tagTable = EasyUIJsonParser.DataGridJsonParser.JsonToDataTable(tags.JsonPickArray("rows"), GetTagTable());
+
+            string[] test = tags.JsonPickArray("rows");
+
+            DataTable dt = GetQuantityUsage(tagTable, startTime, endTime, timeInterval);
+
+            // 获取横坐标（列，时间信息）
+            IList<string> colNames = new List<string>();
+            foreach (DataColumn dc in dt.Columns)
+            {
+                colNames.Add(dc.ColumnName.ToString());
+            }
+
+            // 获取图例（行，数据信息）
+            IList<string> rowName = new List<string>();
+            foreach (DataRow dr in tagTable.Rows)
+            {
+                rowName.Add(dr["Name"].ToString());
+            }
+            string m_UnitX = "";
+            if (timeInterval == "10")
+            {
+                m_UnitX = "月-日-时-分";
+            }
+            else if (timeInterval == "20")
+            {
+                m_UnitX = "月-日-时";
+            }
+            else if (timeInterval == "30")
+            {
+                m_UnitX = "年-月-日";
+            }
+            return ChartJsonParser.GetGridChartJsonString(dt, colNames.ToArray(), rowName.ToArray(), m_UnitX, "kWh", 1);
+        }
+
+        /// <summary>
+        /// 获取标签列表的表格结构
+        /// </summary>
+        /// <returns></returns>
+        private static DataTable GetTagTable()
+        {
+            DataTable tagTable = new DataTable();
+            tagTable.Columns.Add("OrganizationId", typeof(string));
+            tagTable.Columns.Add("TagTableName", typeof(string));
+            tagTable.Columns.Add("TagColumnName", typeof(string));
+            tagTable.Columns.Add("VariableId", typeof(string));
+            tagTable.Columns.Add("Name", typeof(string));
+            return tagTable;
+        }
+
+        /// <summary>
+        /// 获取用电量信息
+        /// </summary>
+        /// <param name="tagTable"></param>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <returns></returns>
+        private static DataTable GetQuantityUsage(DataTable tagTable, string startTime, string endTime, string timeInterval)
+        {
+            #region 参数验证
+            try
+            {
+                DateTime.Parse(startTime);
+                DateTime.Parse(endTime);
+            }
+            catch
+            {
+                throw new ArgumentException("时间参数不正确");
+            }
+            #endregion
+
+            DataTable QuantityUsageSumTable = AmmetersHistoryTrendService.GetQuantityUsageGroup(tagTable, DateTime.Parse(startTime), DateTime.Parse(endTime), timeInterval);
+
+            return TempAnalysisTableHelper.VerticalToHorizontalByInterval(QuantityUsageSumTable, DateTime.Parse(startTime), DateTime.Parse(endTime), timeInterval);
+        }
+
+        [WebMethod]
+        public static string GetAmmetersTreeGridFormat(string organizationId)
+        {
+            DataTable dt = AmmetersHistoryTrendService.GetAmmetersByOrganizationId(organizationId);
+
+            return EasyUIJsonParser.TreeGridJsonParser.DataTableToJson(dt, "KeyId", "Name", "ParentKeyId", "0", new string[] { "OrganizationId", "TagTableName", "TagColumnName", "LevelType", "Name", "VariableId" });
+        }
+    }
+}
